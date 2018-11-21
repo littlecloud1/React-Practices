ea19734ef04862d00ec723050305aa4ef306fcf5
Lai Man Tang <cloudtang030@gmail.com>
2018-11-21 15:16:01 -0800

    feats: switchNameHandler to change state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    //can only called by this props
     state = {
         persons: [
             { name: 'Max', age: 28 },
             { name: 'Manu', age: 29 },
             { name: 'Stephaine', age: 26 }
-        ]     
+        ],
+        otherState: 'some other valuie'
     }
+
+    switchNameHandler = () => {
+        //console.log('Was clicked!');
+        //Don't do this : this.state.person[1].name = 'Nancy'
+
+        //use setState to update
+        //only change on affected state
+        this.setState({
+            persons: [
+                { name: 'Nancy', age: 28 },
+                { name: 'Manu', age: 29 },
+                { name: 'Stephaine', age: 27 }
+            ]
+        });
+
+    }
+
+    //onClick part, the function should not add ()
+    //otherwise it will excute immediately
+    //we only need it when it call 
+    //so pass it as an reference
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
                 <p> This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hoobies: Racing</Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
