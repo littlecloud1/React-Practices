6ba57b78f4d55c91164d23532f8a1543309ec668
Lai Man Tang <cloudtang030@gmail.com>
2018-11-21 14:50:46 -0800

    feats: set up states

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephaine', age: 26 }
+        ]     
+    }
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
                 <p> This is really working!</p>
-                <Person name="Max" age="28"/>
-                <Person name="Manu" age="29" >My Hoobies: Racing</Person>
-                <Person name="Stephanie" age="26"/>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hoobies: Racing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
             //<h1>Another heading</h1> count do this, can only have one root element
         );
