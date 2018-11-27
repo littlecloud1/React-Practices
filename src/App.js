af561659731419ba4d9a869f805bc84593e772b4
Lai Man Tang <cloudtang030@gmail.com>
2018-11-27 01:38:59 -0800

    feats: call function by anoymous functions.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
                 <p> This is really working!</p>
-                <button onClick={this.switchNameHandler.bind(this, 'Bob')}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler("Nancy!!!")}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age} />
