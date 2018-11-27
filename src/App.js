dc257bbed193a0dbeceed0f745a634dd7ce423f3
Lai Man Tang <cloudtang030@gmail.com>
2018-11-27 01:06:08 -0800

    feats: call function by reference

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         otherState: 'some other valuie'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         //console.log('Was clicked!');
         //Don't do this : this.state.person[1].name = 'Nancy'
 
@@ -21,7 +21,7 @@ class App extends Component {
         //only change on affected state
         this.setState({
             persons: [
-                { name: 'Nancy', age: 28 },
+                { name: newName , age: 28 },
                 { name: 'Manu', age: 29 },
                 { name: 'Stephaine', age: 27 }
             ]
@@ -38,10 +38,17 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
                 <p> This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hoobies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={this.switchNameHandler.bind(this, 'Bob')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Nancy')}>My Hoobies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age} />
             </div>
             //<h1>Another heading</h1> count do this, can only have one root element
         );
