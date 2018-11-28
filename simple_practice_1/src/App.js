f28b954c27b8dfaf1e355c33972d320c29244537
Lai Man Tang <cloudtang030@gmail.com>
2018-11-27 21:31:42 -0800

    feats: add toogle person condition.

--- simple_practice_1/src/App.js
+++ simple_practice_1/src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
                 { name: event.target.value, age: 29 },
                 { name: 'Stephaine', age: 26 }
             ],
-            otherState: 'some other valuie'
+            otherState: 'some other valuie',
+            showPersons: false
         });
     }
 
@@ -40,28 +41,50 @@ class App extends Component {
 
     }
 
+    togglePersonHandle = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({ showPersons: !doesShow });
+    }
+
+
+
     //onClick part, the function should not add ()
     //otherwise it will excute immediately
     //we only need it when it call 
     //so pass it as an reference
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'ingerit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
                 <p> This is really working!</p>
-                <button onClick={() => this.switchNameHandler("Nancy!!!")}>Switch Name</button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age} />
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Nancy')}
-                    changed={this.nameChangeHandler}
-                >My Hoobies: Racing</Person>
-                <Person
-                    name={this.state.persons[2].name}
-                    age={this.state.persons[2].age} />
+                <button
+                    style={style}
+                    onClick={this.togglePersonHandle}>Toogle Persons</button>
+                {this.state.showPersons ?
+                    <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age} />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Nancy')}
+                        changed={this.nameChangeHandler}
+                    >My Hoobies: Racing</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age} />
+
+                    </div> : null
+                    }
             </div>
             //<h1>Another heading</h1> count do this, can only have one root element
         );
