daf24243d69aa4de1e1d8f29bc265efc15ba46b3
Lai Man Tang <cloudtang030@gmail.com>
2018-11-27 21:50:58 -0800

    feats: impelement conditions-statement outside return

--- simple_practice_1/src/App.js
+++ simple_practice_1/src/App.js
@@ -61,6 +61,26 @@ class App extends Component {
             cursor: 'pointer'
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (<div>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Nancy')}
+                    changed={this.nameChangeHandler}
+                >My Hoobies: Racing</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age} />
+
+            </div>);
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App.</h1>
@@ -68,23 +88,7 @@ class App extends Component {
                 <button
                     style={style}
                     onClick={this.togglePersonHandle}>Toogle Persons</button>
-                {this.state.showPersons ?
-                    <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age} />
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Nancy')}
-                        changed={this.nameChangeHandler}
-                    >My Hoobies: Racing</Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age} />
-
-                    </div> : null
-                    }
+                {persons}
             </div>
             //<h1>Another heading</h1> count do this, can only have one root element
         );
